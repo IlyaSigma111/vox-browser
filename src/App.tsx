cffd760f6c0f9e0a50d69643d3ead79e85f74766
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-07-27 21:12:23 +0500

    v1.0.1: white icon, workspace ctx menu, Ctrl+Alt+digit, null safety fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import { useStore, THEMES } from './store'
 import { setLang } from './lang'
 import TabBar from './components/TabBar'
